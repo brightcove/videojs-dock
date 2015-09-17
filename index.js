51cc37d96835d6966c1c97777e9795219fd673ad
Gary Katsevman <git@gkatsev.com>
2015-09-17 14:55:09 -0400

    Update with two separate components for title and shelf.

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 let videojs = require('video.js');
 let Component = videojs.getComponent('Component');
 
-export class Dock extends Component {
+export class Title extends Component {
   constructor(player, options) {
     super(player, options);
-    console.log(this.options_);
   }
 
   createEl() {
-    return super.createEl('div', {
-      className: 'vjs-dock',
-      innerHTML: `
-        <h1 class='vjs-dock-title'>${this.options_.title}</h1>
-      `
+    return super.createEl('h1', {
+      className: 'vjs-dock-title',
+      innerHTML: this.options_.title
     });
   }
 };
 
+export class Shelf extends Component {
+  createEl() {
+    return super.createEl('div', {
+      className: 'vjs-dock-shelf'
+    });
+  }
+};
+
+
+export class Dock extends Component {
+  constructor(player, options) {
+    super(player, options);
+  }
+
+  createEl() {
+    return super.createEl('div', {
+      className: 'vjs-dock'
+    });
+  }
+
+};
+
+
+Dock.prototype.options_ = {
+  children: [
+    'title',
+    'shelf'
+  ]
+};
+
+videojs.registerComponent('Title', Title);
+videojs.registerComponent('Shelf', Shelf);
 videojs.registerComponent('Dock', Dock);
 
 videojs.plugin('dock', function(options) {
-  this.addChild('dock', options);
+  let settings = {
+    title: {
+      title: options.title
+    }
+  };
+  let dock = this.addChild('dock', settings);
 });
